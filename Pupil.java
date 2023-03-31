7a7d61fca901f9963aeee61c767f6c4f24e88d44
Mohammed Uzair Ahmed <97060605+uzairahmed4@users.noreply.github.com>
2023-03-31 17:18:11 +0400

    Update Pupil.java

--- Pupil.java
+++ Pupil.java
@@ -4,12 +4,20 @@
 public class Pupil extends Person {
 
 	/**
-	 * This represents the parent or guardian of a pupil who is the primary contact with the school. It is expected that this person will keep other parents or guardians informed, and that they will attend the parents evening together if both (or all) wish to attend. This reflects how schools generally operate when parents are separated or divorced.
+	 * This represents the parent or guardian of a pupil who is the primary contact with the school. It is expected that this person will keep 
+         * other parents or guardians informed, and that they will attend the parents evening together if both (or all) wish to attend. 
+         * This reflects how schools generally operate when parents are separated or divorced.
 	 */
 	private Person contactParent;
 	/**
 	 * The teachers who teach this pupil.
 	 */
 	private Teacher[] taughtBy;
+        
+//        public void addName () {
+//            contactParent = new Person("john"); 
+//        }
+        
 
+        
 }

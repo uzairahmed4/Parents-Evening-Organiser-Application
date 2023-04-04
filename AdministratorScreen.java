00a9f3d563a0fff07ba31acf34f20f6226f3f3de
Shantanu0107 <126323117+Shantanu0107@users.noreply.github.com>
2023-04-04 18:42:52 +0400

    Update AdministratorScreen.java
    Updated GUI for admin screen class

--- AdministratorScreen.java
+++ AdministratorScreen.java
@@ -1,8 +1,5 @@
-/**
- * This is the interface used by the PEO system administrator.
- * It is a Boundary class.
- * In terms of the MVC pattern, it is also a View and a Controller.
- */
+package SE;
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -21,11 +18,18 @@ import javax.swing.JComboBox;
 import javax.swing.JCheckBox;
 import javax.swing.JSlider;
 import javax.swing.SwingConstants;
+import java.awt.Label;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class AdministratorScreen extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField studentNameText;
+	private JTextField textField;
+	private JTable pupilTable;
+	private JTable teacherTable;
 
 	/**
 	 * Launch the application.
@@ -48,31 +52,31 @@ public class AdministratorScreen extends JFrame {
 	 */
 	public AdministratorScreen() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 821, 576);
+		setBounds(100, 100, 890, 879);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
-		TextArea ResultTextArea = new TextArea();
-		ResultTextArea.setForeground(Color.WHITE);
-		ResultTextArea.setFont(new Font("Dialog", Font.BOLD, 15));
-		ResultTextArea.setBackground(Color.DARK_GRAY);
-		ResultTextArea.setBounds(67, 238, 664, 174);
-		contentPane.add(ResultTextArea);
+		TextArea LogTextArea = new TextArea();
+		LogTextArea.setForeground(Color.WHITE);
+		LogTextArea.setFont(new Font("Dialog", Font.BOLD, 15));
+		LogTextArea.setBackground(new Color(255, 255, 255));
+		LogTextArea.setBounds(187, 538, 424, 265);
+		contentPane.add(LogTextArea);
 		
 		studentNameText = new JTextField();
 		studentNameText.setToolTipText("Enter minimum budget");
 		studentNameText.setFont(new Font("Tahoma", Font.BOLD, 13));
 		studentNameText.setColumns(10);
-		studentNameText.setBounds(187, 197, 134, 20);
+		studentNameText.setBounds(187, 242, 134, 20);
 		contentPane.add(studentNameText);
 		
-		JLabel lblEnterName = new JLabel("Enter Name:");
+		JLabel lblEnterName = new JLabel("Enter Pupil Name:");
 		lblEnterName.setBackground(Color.GRAY);
 		lblEnterName.setForeground(Color.BLACK);
 		lblEnterName.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblEnterName.setBounds(67, 196, 110, 20);
+		lblEnterName.setBounds(38, 241, 139, 20);
 		contentPane.add(lblEnterName);
 		
 		JButton btnDisplayLog = new JButton("Display Log");
@@ -83,147 +87,176 @@ public class AdministratorScreen extends JFrame {
 		btnDisplayLog.setToolTipText("Search Budget");
 		btnDisplayLog.setFont(new Font("Tahoma", Font.BOLD, 10));
 		btnDisplayLog.setBackground(new Color(135, 206, 250));
-		btnDisplayLog.setBounds(621, 198, 110, 23);
+		btnDisplayLog.setBounds(67, 713, 110, 23);
 		contentPane.add(btnDisplayLog);
-		
-		JComboBox<Object> selectTypeCombo = new JComboBox<Object>(new Object[]{});
-		selectTypeCombo.setSelectedIndex(-1);
-		selectTypeCombo.setForeground(Color.WHITE);
-		selectTypeCombo.setFont(new Font("Dubai", Font.BOLD, 14));
-		selectTypeCombo.setBackground(new Color(51, 51, 51));
-		selectTypeCombo.setBounds(469, 199, 128, 20);
-		contentPane.add(selectTypeCombo);
-		
-		JLabel lblSelectType = new JLabel("Select Type :");
-		lblSelectType.setForeground(Color.BLACK);
-		lblSelectType.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblSelectType.setBackground(Color.GRAY);
-		lblSelectType.setBounds(349, 197, 110, 20);
-		contentPane.add(lblSelectType);
 		
 		JLabel lblBookingsOpen = new JLabel("Bookings Open:");
 		lblBookingsOpen.setForeground(Color.BLACK);
 		lblBookingsOpen.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblBookingsOpen.setBackground(Color.GRAY);
-		lblBookingsOpen.setBounds(38, 27, 116, 20);
+		lblBookingsOpen.setBounds(24, 42, 116, 20);
 		contentPane.add(lblBookingsOpen);
 		
 		JLabel lblEveningStart = new JLabel("Evening Start:");
 		lblEveningStart.setForeground(Color.BLACK);
 		lblEveningStart.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblEveningStart.setBackground(Color.GRAY);
-		lblEveningStart.setBounds(426, 27, 110, 20);
+		lblEveningStart.setBounds(24, 101, 110, 20);
 		contentPane.add(lblEveningStart);
 		
 		JLabel lblBookingsClose = new JLabel("Bookings Close:");
 		lblBookingsClose.setForeground(Color.BLACK);
 		lblBookingsClose.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblBookingsClose.setBackground(Color.GRAY);
-		lblBookingsClose.setBounds(38, 73, 126, 20);
+		lblBookingsClose.setBounds(298, 42, 126, 20);
 		contentPane.add(lblBookingsClose);
 		
 		JLabel lblEveningEnd = new JLabel("Evening End:");
 		lblEveningEnd.setForeground(Color.BLACK);
 		lblEveningEnd.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblEveningEnd.setBackground(Color.GRAY);
-		lblEveningEnd.setBounds(426, 73, 110, 20);
+		lblEveningEnd.setBounds(298, 101, 110, 20);
 		contentPane.add(lblEveningEnd);
 		
-		JComboBox<Object> bookOpenComboHr = new JComboBox<Object>(new Object[]{});
-		bookOpenComboHr.setSelectedIndex(-1);
-		bookOpenComboHr.setForeground(Color.WHITE);
-		bookOpenComboHr.setFont(new Font("Dubai", Font.BOLD, 14));
-		bookOpenComboHr.setBackground(new Color(51, 51, 51));
-		bookOpenComboHr.setBounds(165, 29, 67, 20);
-		contentPane.add(bookOpenComboHr);
+		JComboBox<Object> bookOpenTime = new JComboBox<Object>(new Object[]{});
+		bookOpenTime.setSelectedIndex(-1);
+		bookOpenTime.setForeground(Color.WHITE);
+		bookOpenTime.setFont(new Font("Dubai", Font.BOLD, 14));
+		bookOpenTime.setBackground(new Color(51, 51, 51));
+		bookOpenTime.setBounds(158, 42, 110, 20);
+		contentPane.add(bookOpenTime);
 		
-		JComboBox<Object> bookOpenComboMins = new JComboBox<Object>(new Object[]{});
-		bookOpenComboMins.setSelectedIndex(-1);
-		bookOpenComboMins.setForeground(Color.WHITE);
-		bookOpenComboMins.setFont(new Font("Dubai", Font.BOLD, 14));
-		bookOpenComboMins.setBackground(new Color(51, 51, 51));
-		bookOpenComboMins.setBounds(254, 29, 67, 20);
-		contentPane.add(bookOpenComboMins);
+		JComboBox<Object> bookCloseTime = new JComboBox<Object>(new Object[]{});
+		bookCloseTime.setSelectedIndex(-1);
+		bookCloseTime.setForeground(Color.WHITE);
+		bookCloseTime.setFont(new Font("Dubai", Font.BOLD, 14));
+		bookCloseTime.setBackground(new Color(51, 51, 51));
+		bookCloseTime.setBounds(434, 42, 110, 20);
+		contentPane.add(bookCloseTime);
 		
-		JComboBox<Object> bookCloseComboHr = new JComboBox<Object>(new Object[]{});
-		bookCloseComboHr.setSelectedIndex(-1);
-		bookCloseComboHr.setForeground(Color.WHITE);
-		bookCloseComboHr.setFont(new Font("Dubai", Font.BOLD, 14));
-		bookCloseComboHr.setBackground(new Color(51, 51, 51));
-		bookCloseComboHr.setBounds(165, 75, 67, 20);
-		contentPane.add(bookCloseComboHr);
+		JComboBox<Object> eveningStartTime = new JComboBox<Object>(new Object[]{});
+		eveningStartTime.setSelectedIndex(-1);
+		eveningStartTime.setForeground(Color.WHITE);
+		eveningStartTime.setFont(new Font("Dubai", Font.BOLD, 14));
+		eveningStartTime.setBackground(new Color(51, 51, 51));
+		eveningStartTime.setBounds(158, 101, 110, 20);
+		contentPane.add(eveningStartTime);
 		
-		JComboBox<Object> bookCloseComboMins = new JComboBox<Object>(new Object[]{});
-		bookCloseComboMins.setSelectedIndex(-1);
-		bookCloseComboMins.setForeground(Color.WHITE);
-		bookCloseComboMins.setFont(new Font("Dubai", Font.BOLD, 14));
-		bookCloseComboMins.setBackground(new Color(51, 51, 51));
-		bookCloseComboMins.setBounds(254, 75, 67, 20);
-		contentPane.add(bookCloseComboMins);
-		
-		JComboBox<Object> eveningStartComboHr = new JComboBox<Object>(new Object[]{});
-		eveningStartComboHr.setSelectedIndex(-1);
-		eveningStartComboHr.setForeground(Color.WHITE);
-		eveningStartComboHr.setFont(new Font("Dubai", Font.BOLD, 14));
-		eveningStartComboHr.setBackground(new Color(51, 51, 51));
-		eveningStartComboHr.setBounds(546, 29, 67, 20);
-		contentPane.add(eveningStartComboHr);
-		
-		JComboBox<Object> eveningStartComboMins = new JComboBox<Object>(new Object[]{});
-		eveningStartComboMins.setSelectedIndex(-1);
-		eveningStartComboMins.setForeground(Color.WHITE);
-		eveningStartComboMins.setFont(new Font("Dubai", Font.BOLD, 14));
-		eveningStartComboMins.setBackground(new Color(51, 51, 51));
-		eveningStartComboMins.setBounds(634, 29, 67, 20);
-		contentPane.add(eveningStartComboMins);
-		
-		JComboBox<Object> eveningEndComboHr = new JComboBox<Object>(new Object[]{});
-		eveningEndComboHr.setSelectedIndex(-1);
-		eveningEndComboHr.setForeground(Color.WHITE);
-		eveningEndComboHr.setFont(new Font("Dubai", Font.BOLD, 14));
-		eveningEndComboHr.setBackground(new Color(51, 51, 51));
-		eveningEndComboHr.setBounds(546, 75, 67, 20);
-		contentPane.add(eveningEndComboHr);
-		
-		JComboBox<Object> eveningEndComboMins = new JComboBox<Object>(new Object[]{});
-		eveningEndComboMins.setSelectedIndex(-1);
-		eveningEndComboMins.setForeground(Color.WHITE);
-		eveningEndComboMins.setFont(new Font("Dubai", Font.BOLD, 14));
-		eveningEndComboMins.setBackground(new Color(51, 51, 51));
-		eveningEndComboMins.setBounds(634, 75, 67, 20);
-		contentPane.add(eveningEndComboMins);
-		
-		JCheckBox verifyCheckBox = new JCheckBox("Agree to Terms and condition");
-		verifyCheckBox.setForeground(Color.WHITE);
-		verifyCheckBox.setFont(new Font("Dubai", Font.ITALIC, 11));
-		verifyCheckBox.setBackground(new Color(51, 51, 51));
-		verifyCheckBox.setBounds(67, 437, 176, 24);
-		contentPane.add(verifyCheckBox);
+		JComboBox<Object> eveningEndTime = new JComboBox<Object>(new Object[]{});
+		eveningEndTime.setSelectedIndex(-1);
+		eveningEndTime.setForeground(Color.WHITE);
+		eveningEndTime.setFont(new Font("Dubai", Font.BOLD, 14));
+		eveningEndTime.setBackground(new Color(51, 51, 51));
+		eveningEndTime.setBounds(434, 101, 110, 20);
+		contentPane.add(eveningEndTime);
 		
 		JButton btnClearLog = new JButton("Clear Log");
 		btnClearLog.setToolTipText("Search Budget");
 		btnClearLog.setFont(new Font("Tahoma", Font.BOLD, 10));
 		btnClearLog.setBackground(new Color(135, 206, 250));
-		btnClearLog.setBounds(67, 479, 110, 23);
+		btnClearLog.setBounds(617, 713, 110, 23);
 		contentPane.add(btnClearLog);
-		
-		JSlider meetDurationSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 70);
-		meetDurationSlider.setInverted(true);
-		meetDurationSlider.setBounds(228, 132, 385, 20);
-		contentPane.add(meetDurationSlider);
 		
 		JLabel lblMeetingDuration = new JLabel("Meeting Duration:");
 		lblMeetingDuration.setForeground(Color.BLACK);
 		lblMeetingDuration.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblMeetingDuration.setBackground(Color.GRAY);
-		lblMeetingDuration.setBounds(79, 132, 139, 20);
+		lblMeetingDuration.setBounds(24, 164, 139, 20);
 		contentPane.add(lblMeetingDuration);
 		
-		JLabel meetDurationTime = new JLabel("-min(s)");
-		meetDurationTime.setForeground(Color.BLACK);
-		meetDurationTime.setFont(new Font("Tahoma", Font.BOLD, 15));
-		meetDurationTime.setBackground(Color.GRAY);
-		meetDurationTime.setBounds(374, 154, 57, 20);
-		contentPane.add(meetDurationTime);
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(162, 165, 92, 22);
+		contentPane.add(comboBox);
+		
+		JButton AddPupilBtn = new JButton("Add Pupil");
+		AddPupilBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		AddPupilBtn.setBounds(187, 273, 104, 23);
+		contentPane.add(AddPupilBtn);
+		
+		JButton EditPupilBtn = new JButton("Edit Pupil");
+		EditPupilBtn.setBounds(187, 307, 104, 23);
+		contentPane.add(EditPupilBtn);
+		
+		JButton RemovePupilBtn = new JButton("Remove pupil");
+		RemovePupilBtn.setBounds(187, 341, 104, 23);
+		contentPane.add(RemovePupilBtn);
+		
+		textField = new JTextField();
+		textField.setBounds(546, 243, 144, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Enter Teacher Name:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setBounds(373, 244, 163, 14);
+		contentPane.add(lblNewLabel);
+		
+		JButton AddTeacherBtn = new JButton("Add Teacher");
+		AddTeacherBtn.setBounds(546, 273, 116, 23);
+		contentPane.add(AddTeacherBtn);
+		
+		JButton EditTeacherBtn = new JButton("Edit Teacher");
+		EditTeacherBtn.setBounds(546, 307, 116, 23);
+		contentPane.add(EditTeacherBtn);
+		
+		JButton RemoveTeacherBtn = new JButton("Remove Teacher");
+		RemoveTeacherBtn.setBounds(546, 341, 116, 23);
+		contentPane.add(RemoveTeacherBtn);
+		
+		JButton BookingsSetBtn = new JButton("Booking Set");
+		BookingsSetBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		BookingsSetBtn.setBounds(573, 43, 89, 23);
+		contentPane.add(BookingsSetBtn);
+		
+		JButton EveningSetBtn = new JButton("Evening Set");
+		EveningSetBtn.setBounds(573, 102, 89, 23);
+		contentPane.add(EveningSetBtn);
+		
+		JButton SetDurationBtn = new JButton("Set Duration");
+		SetDurationBtn.setBounds(287, 165, 110, 23);
+		contentPane.add(SetDurationBtn);
+		
+		JScrollPane pupilScrollPane = new JScrollPane();
+		pupilScrollPane.setBounds(187, 393, 110, 123);
+		contentPane.add(pupilScrollPane);
+		
+		pupilTable = new JTable();
+		pupilTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+			},
+			new String[] {
+				"Pupil Name"
+			}
+		));
+		pupilScrollPane.setViewportView(pupilTable);
+		
+		JScrollPane teacherScrollPane = new JScrollPane();
+		teacherScrollPane.setBounds(546, 393, 126, 75);
+		contentPane.add(teacherScrollPane);
+		
+		teacherTable = new JTable();
+		teacherTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null},
+				{null},
+				{null},
+			},
+			new String[] {
+				"Teacher Name"
+			}
+		));
+		teacherScrollPane.setViewportView(teacherTable);
 	}
 }
